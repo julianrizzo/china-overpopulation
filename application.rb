a6e3fa38bf0c2bcbd5fa397d54d78e9ec9d26405
Julian Rizzo <Julian@Julians-MBP.home>
2016-05-26 21:25:58 +1000

    i think this might work

--- application.rb
+++ application.rb
@@ -20,7 +20,7 @@ class App < Sinatra::Base
 	set :public_folder, File.dirname(__FILE__) + '/public/'
 	set :assets_prefix, %w(app/assets vendor/assets)
 
-	set :assets_precompile, %w(application.js application.css *.png *.jpg *.svg *.eot *.ttf *.woff)
+	set :assets_precompile, %w(application.js application.css vendor.js vendor.css *.png *.jpg *.svg *.eot *.ttf *.woff)
 
 	set :assets_css_compressor, :sass
 
